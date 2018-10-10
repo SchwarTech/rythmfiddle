0f62e458e683fe8fcce05f638bc23843ccc05f93
Jeff Schwartz <jeff@schwartz-tribe.com>
2018-10-10 15:03:33 -0400

    Made to work with play-1.5.1 and RythmEngine-1.0.1-b3

--- README.md
+++ README.md
@@ -3,9 +3,12 @@ Use Play 1.2.5
 How to run
 ----------
 
+1. Download play 1.5.1 (https://www.playframework.com/releases)
+1. Put path to play in your PATH
 1. `play deps --sync`
 1. go to module `app-base`, and delete the file `UADetector.java`
+   rm modules/app-base-2.1.3/app/controllers/filters/UADetector.java 
 1. `cp conf/account.txt.sample conf/account.txt`
 1. add `username=password` list there
 1. `play run`
-1. visit `http://localhost:9000`
+1. visit `http://localhost:8080`
--- app/jobs/InitJobs.java
+++ app/jobs/InitJobs.java
@@ -25,10 +25,39 @@ public class InitJobs extends Job {
     public void doJob() throws Exception {
         CodeManager.CODE_ROOT.mkdirs();
         CodeManager.reload();
-        loadModelClassesBeforeHand();
+//        loadModelClassesBeforeHand();
         //Code.initRythmEngine();
     }
 
+    /**
+     * Since I am not using this demo for implicitVariables, I have
+     * commented out the code that calls this method.
+     * 
+     * This method causes the following error
+     *
+     * Error during the 500 response generation
+     * java.lang.NoSuchMethodError: play.Play.plugin(Ljava/lang/Class;)Ljava/lang/Object;
+     * 	at org.rythmengine.play.ImplicitVariables$5.evaluate(ImplicitVariables.java:76)
+     * 	at org.rythmengine.play.RythmPlugin$8.setRenderArgs(RythmPlugin.java:480)
+     * 	at org.rythmengine.internal.EventBus$7.handleEvent(EventBus.java:239)
+     * 	at org.rythmengine.internal.EventBus$7.handleEvent(EventBus.java:234)
+     * 	at org.rythmengine.internal.EventBus.accept(EventBus.java:156)
+     * 	at org.rythmengine.RythmEngine.accept(RythmEngine.java:1910)
+     * 	at org.rythmengine.internal.RythmEvents.trigger(RythmEvents.java:138)
+     * 	at org.rythmengine.template.TagBase.__triggerRenderEvent(TagBase.java:93)
+     * 	at org.rythmengine.template.TemplateBase.render(TemplateBase.java:594)
+     * 	at org.rythmengine.play.RythmTemplate.internalRender(RythmTemplate.java:151)
+     * 	at play.templates.Template.render(Template.java:28)
+     * 	at play.server.PlayHandler.serve500(PlayHandler.java:785)
+     * 	at play.server.PlayHandler$NettyInvocation.run(PlayHandler.java:254)
+     * 	at java.util.concurrent.Executors$RunnableAdapter.call(Executors.java:511)
+     * 	at java.util.concurrent.FutureTask.run(FutureTask.java:266)
+     * 	at java.util.concurrent.ScheduledThreadPoolExecutor$ScheduledFutureTask.access$201(ScheduledThreadPoolExecutor.java:180)
+     * 	at java.util.concurrent.ScheduledThreadPoolExecutor$ScheduledFutureTask.run(ScheduledThreadPoolExecutor.java:293)
+     * 	at java.util.concurrent.ThreadPoolExecutor.runWorker(ThreadPoolExecutor.java:1149)
+     * 	at java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:624)
+     * 	at java.lang.Thread.run(Thread.java:748)
+     */
     private void loadModelClassesBeforeHand() {
         new Bar();
         new Foo();
